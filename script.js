71a4ac53a2776a232b9510d737a8741bc4db7800
Elijah-glitch <62041760+Elijah-glitch@users.noreply.github.com>
2023-08-13 03:57:43 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ function updateCountdown() {
     const targetDate = new Date(now.getTime() + 2 * 24 * 60 * 60 * 1000); // 2 days in milliseconds
     const timeRemaining = targetDate - now;
 
-    const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+    const secondsRemaining = Math.floor(timeRemaining / 1000);
+
+    const days = Math.floor(secondsRemaining / (24 * 60 * 60));
+    const hours = Math.floor((secondsRemaining % (24 * 60 * 60)) / (60 * 60));
+    const minutes = Math.floor((secondsRemaining % (60 * 60)) / 60);
+    const seconds = secondsRemaining % 60;
 
     document.getElementById("days").textContent = days.toString().padStart(2, "0");
     document.getElementById("hours").textContent = hours.toString().padStart(2, "0");
